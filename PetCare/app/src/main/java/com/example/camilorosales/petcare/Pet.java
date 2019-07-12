65eba74efee65d6d25bbf344611184a76c2ad2e7
marraui <camilomarraui@gmail.com>
2019-07-12 11:39:40 -0500

    Added Temperature and HeartRate attributes

--- PetCare/app/src/main/java/com/example/camilorosales/petcare/Pet.java
+++ PetCare/app/src/main/java/com/example/camilorosales/petcare/Pet.java
@@ -8,10 +8,19 @@ public class Pet implements Serializable {
 
     private String mName;
     private int mTypeOfPet;
+    private float mTemperature;
+    private float mHeartRate;
 
     public Pet(String name, int typeOfPet){
         mName = name;
         mTypeOfPet = typeOfPet;
+    }
+
+    public Pet(String name, int typeOfPet, float temperature, float hearRate) {
+        mName = name;
+        mTypeOfPet = typeOfPet;
+        mTemperature = temperature;
+        mHeartRate = hearRate;
     }
 
     public String getName(){
@@ -24,5 +33,21 @@ public class Pet implements Serializable {
 
     public void setName(String name){
         mName = name;
+    }
+
+    public float getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(float mTemperature) {
+        this.mTemperature = mTemperature;
+    }
+
+    public float getHeartRate() {
+        return mHeartRate;
+    }
+
+    public void setHeartRate(float mHeartRate) {
+        this.mHeartRate = mHeartRate;
     }
 }
