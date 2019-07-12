b8f0ad7c4a752b2984d943cb8b60fa6a76606cb1
marraui <camilomarraui@gmail.com>
2019-07-12 11:41:40 -0500

    Draw demo charts

--- PetCare/app/src/main/java/com/example/camilorosales/petcare/PetDetailsActivity.java
+++ PetCare/app/src/main/java/com/example/camilorosales/petcare/PetDetailsActivity.java
@@ -1,11 +1,26 @@
 package com.example.camilorosales.petcare;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+
 public class PetDetailsActivity extends AppCompatActivity {
+
+    private LineChart mTemperatureChart;
+    private LineChart mHeartRateChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +31,97 @@ public class PetDetailsActivity extends AppCompatActivity {
         Bundle bundle = intent.getExtras();
         Pet pet = (Pet) bundle.getSerializable("pet");
 
-        TextView name = (TextView) findViewById(R.id.pet_details_name);
-        name.setText(pet.getName());
+        mTemperatureChart = findViewById(R.id.temperature_chart);
+        mHeartRateChart = findViewById(R.id.hear_rate_chart);
+
+        // Chart style
+        setChartStyle(mTemperatureChart);
+        setChartStyle(mHeartRateChart);
+
+        //refresh
+        mTemperatureChart.invalidate();
+        mHeartRateChart.invalidate();
+
+        mTemperatureChart.setData(getTemperatureData(pet));
+        mHeartRateChart.setData(getHeartRateData(pet));
+
+    }
+
+    private void setChartStyle(LineChart lineChart) {
+        // Controlling X axis
+        XAxis xAxis = lineChart.getXAxis();
+        // Set the xAxis position to bottom. Default is top
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        // Controlling right side of y axis
+        YAxis yAxisRight = lineChart.getAxisRight();
+        yAxisRight.setEnabled(false);
+
+        // disable description text
+        lineChart.getDescription().setEnabled(false);
+
+        // enable touch gestures
+        lineChart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        lineChart.setDragEnabled(true);
+        lineChart.setScaleEnabled(true);
+
+        // force pinch zoom along both axis
+        lineChart.setPinchZoom(true);
+
+        lineChart.getXAxis().setDrawGridLines(false);
+    }
+
+    private LineData getTemperatureData(Pet pet) {
+        ArrayList<Entry> lineEntries = new ArrayList<>();
+        lineEntries.add(new Entry(0, 1));
+        lineEntries.add(new Entry(1, 2));
+        lineEntries.add(new Entry(2, 3));
+        lineEntries.add(new Entry(3, 4));
+        lineEntries.add(new Entry(4, 2));
+        lineEntries.add(new Entry(5, 3));
+        lineEntries.add(new Entry(6, 1));
+        lineEntries.add(new Entry(7, 5));
+        lineEntries.add(new Entry(8, 7));
+        lineEntries.add(new Entry(9, 6));
+        lineEntries.add(new Entry(10, 4));
+        lineEntries.add(new Entry(11, 5));
+
+        LineDataSet lineDataSet = new LineDataSet(lineEntries, getString(R.string.temperature));
+        lineDataSet.setColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        lineDataSet.setValueTextColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        lineDataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        lineDataSet.setDrawValues(false);
+        lineDataSet.setDrawCircles(false);
+
+        LineData lineData = new LineData(lineDataSet);
+        return lineData;
+    }
+
+    private LineData getHeartRateData(Pet pet) {
+        ArrayList<Entry> lineEntries = new ArrayList<>();
+        lineEntries.add(new Entry(0, 1));
+        lineEntries.add(new Entry(1, 2));
+        lineEntries.add(new Entry(2, 3));
+        lineEntries.add(new Entry(3, 4));
+        lineEntries.add(new Entry(4, 2));
+        lineEntries.add(new Entry(5, 3));
+        lineEntries.add(new Entry(6, 1));
+        lineEntries.add(new Entry(7, 5));
+        lineEntries.add(new Entry(8, 7));
+        lineEntries.add(new Entry(9, 6));
+        lineEntries.add(new Entry(10, 4));
+        lineEntries.add(new Entry(11, 5));
+
+        LineDataSet lineDataSet = new LineDataSet(lineEntries, getString(R.string.heart_rate));
+        lineDataSet.setColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        lineDataSet.setValueTextColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        lineDataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        lineDataSet.setDrawValues(false);
+        lineDataSet.setDrawCircles(false);
+
+        LineData lineData = new LineData(lineDataSet);
+        return lineData;
     }
 }
